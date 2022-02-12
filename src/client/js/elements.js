b57e6e92de5451d42c66d3eac13322841d2866bf
Your Name <m.braslava@gmail.com>
2022-02-12 20:41:07 +0200

    writing out pseudocode

--- src/client/js/elements.js
+++ src/client/js/elements.js
@@ -2,3 +2,6 @@
 export const form = document.querySelector('.app__form');
 // seect the loader
 export const loader = document.querySelector('.loader');
+// slect save trip button
+
+// select remove trip button
--- src/client/js/lib.js
+++ src/client/js/lib.js
@@ -1,6 +1,27 @@
 import { wait } from './helpers';
 import { loader } from './elements';
 
+// Select user's form input - location and date
+
+// Convert the location name to latitude and longitude uising Geonames API
+// Send the location to server in a POST request
+
+// Use the latitude and longitude to get weather forecast from Weatherbit
+
+// Calculate how many days away the departure is.
+
+// Check if it is more than 7 days
+
+// If true get current weather forecast
+
+// If false - get future wether forecasst
+
+// If it is more than 16 days away, display weather on day 16th with a note that forecast after date X is not available
+
+// retrieve and image of the location from Pixabay API
+
+// update UI with the location, days until the trip, weather forecast and an image of the destination
+
 export async function postData(url = '', data = {}) {
 	// display a loader while fetching data
 	loader.classList.remove('hidden');
