7ca263bd6c8f22cf16d20f5cd145bec7b480bc38
Your Name <m.braslava@gmail.com>
2022-02-24 15:47:58 +0200

    removing unnecessary comments

--- webpack.prod.js
+++ webpack.prod.js
@@ -70,8 +70,6 @@ module.exports = {
 		}),
 		new MiniCssExtractPlugin({ filename: '[name].css' }),
 		new WorkboxPlugin.GenerateSW({
-			// these options encourage the ServiceWorkers to get in there fast
-			// and not allow any straggling "old" SWs to hang around
 			clientsClaim: true,
 			skipWaiting: true,
 		}),
